87c0ef5bdd02453dedee4d332059c333d6c10493
Zibo Liu <zibo.liu@momenta.ai>
2024-04-23 16:06:01 +0800

    feat: initial.

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const { Server, WebSocket } = require("ws");
+
+const app = express();
+const PORT = 8000;
+
+app.use(express.static("public"));
+
+const server = app.listen(PORT, () => {
+  console.log(`HTTP 服务器运行在 http://localhost:${PORT}`);
+});
+
+const wss = new Server({ server });
+
+wss.on("connection", (ws) => {
+  console.log("客户端已连接");
+
+  ws.on("message", (message) => {
+    console.log(`收到消息: ${message}`);
+  });
+});
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+app.post("/send-message", express.json(), (req, res) => {
+  const message = req.body.message;
+
+  // 广播消息给所有连接的客户端
+  wss.clients.forEach((client) => {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(message);
+    }
+  });
+
+  res.send("消息已发送");
+});
