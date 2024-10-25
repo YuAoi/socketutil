5b79bd3311ab0547340144ae581f52154d8a2563
Zibo Liu <zibo.liu@momenta.ai>
2024-10-25 10:59:47 +0800

    feat: 修改 socket 端口

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { Server, WebSocket } = require("ws");
 
 const app = express();
-const PORT = 8000;
+const PORT = 5001;
 
 app.use(express.static("public"));
 
